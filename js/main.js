75f685f5f71f25bc9047501065e0cf5d73181955
poonggyeongdam-2026 <punggyeongdam@gmail.com>
2026-07-01 13:56:06 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -48,3 +48,23 @@ body {
   transition: 0.3s;
   background: #0d5a43;
 }
+const counters = document.querySelectorAll('.count');
+
+counters.forEach(counter => {
+  counter.innerText = '0';
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute('data-target');
+    const current = +counter.innerText;
+    const increment = target / 100;
+
+    if (current < target) {
+      counter.innerText = Math.ceil(current + increment);
+      setTimeout(updateCounter, 20);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCounter();
+});
